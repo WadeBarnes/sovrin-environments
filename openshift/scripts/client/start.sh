72d3451b332181e22f2b1ac7d385310e23a5d632
Wade Barnes <wade.barnes@shaw.ca>
2017-09-21 11:58:34 -0700

    Keep the the client pod running.

--- openshift/scripts/client/start.sh
+++ openshift/scripts/client/start.sh
@@ -3,7 +3,11 @@ SCRIPT_DIR=$(dirname $0)
 
 $SCRIPT_DIR/initialize.sh
 
-echo "Starting sovrin client ..."
-echo
 
-sovrin
+
+echo "Starting sovrin client node ..."
+echo "The sovrin cli will not keep the pod running, so instead we'll sleep for infinity."
+echo "To use the sovrin cli, rsh into the pod and run the cli in the session."
+echo
+sleep infinity
+# sovrin
